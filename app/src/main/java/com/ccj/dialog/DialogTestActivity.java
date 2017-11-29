fe8a0ebea2c035de0d400a58dbf6b03c736d9a42
chenchangjun <569948231@qq.com>
2017-11-29 20:19:45 +0800

    Initial commit

---
+++ app/src/main/java/com/ccj/dialog/DialogTestActivity.java
@@ -0,0 +1,157 @@
+package com.ccj.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.ccj.dialoglib.DialogCreator;
+import com.ccj.dialoglib.listener.OnLeftListener;
+import com.ccj.dialoglib.listener.OnRightListener;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+/**
+ * Created by chenchangjun on 17/10/10.
+ */
+
+public class DialogTestActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String url = "https://w4.hoopchina.com.cn/46/cb/04/46cb04b0cbfa425d0281b5a5dbf77d5a002.png";
+
+    Button show_confirm, show_2btn, show_title_2btn, show_edit_title_1btn, show_image_header_1btn, show_logo_header_1btn, show_card_2btn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_test);
+        Fresco.initialize(this);
+
+        show_confirm = (Button) findViewById(R.id.show_confirm);
+        show_2btn = (Button) findViewById(R.id.show_2btn);
+        show_title_2btn = (Button) findViewById(R.id.show_title_2btn);
+        show_edit_title_1btn = (Button) findViewById(R.id.show_edit_title_1btn);
+        show_image_header_1btn = (Button) findViewById(R.id.show_image_header_1btn);
+        show_logo_header_1btn = (Button) findViewById(R.id.show_logo_header_1btn);
+        show_card_2btn = (Button) findViewById(R.id.show_card_2btn);
+
+        show_confirm.setOnClickListener(this);
+        show_2btn.setOnClickListener(this);
+        show_title_2btn.setOnClickListener(this);
+        show_edit_title_1btn.setOnClickListener(this);
+        show_image_header_1btn.setOnClickListener(this);
+        show_logo_header_1btn.setOnClickListener(this);
+        show_card_2btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.show_confirm:
+
+//1
+                DialogCreator.showConfirmDialog(this, "是否退出", "确定", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }).show();
+                break;
+//2
+            case R.id.show_title_2btn:
+
+                DialogCreator.showTitle2BtnDialog(this, "showTitle1BtnDialog",
+
+                        "另外还有客服功能，就是顾客接入咨询时的客服分配，按轮询方式把顾客分配给在线的客服接待。 用开源 Mina 框架实现了 TCP 的长连接接入，用\n" +
+                                "\n" +
+                                "Tomcat Comet 机制实现了 HTTP 的长轮询服务。 而消息投递的实现是一端发送的消息临时存放在 Redis\n" +
+                                "\n" +
+                                "中，另一端拉取的生产消费模型。\n" +
+                                "\n" +
+                                "这个模型的做法导致需要以一种高频率的方式来轮询 Redis 遍历属于自己连接的关联会话消息。\n" +
+                                "\n" +
+                                "\n" ,
+                        "确定", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }, "取消", new OnRightListener() {
+                    @Override
+                    public void onRightClick(String str) {
+
+                    }
+                }).show();
+                break;
+            case R.id.show_2btn:
+//3
+                DialogCreator.show2BtnDialog(this, "show2BtnDialog msg",
+                        "确定", new OnLeftListener() {
+                            @Override
+                            public void onLeftClick(String str) {
+
+                            }
+                        }, "取消", new OnRightListener() {
+                            @Override
+                            public void onRightClick(String str) {
+
+                            }
+                        }).show();
+                break;
+
+//4
+            case R.id.show_edit_title_1btn:
+
+                DialogCreator.showEditTitle1BtnDialog(this, "showEditTitle1BtnDialog", "showEditTitle1BtnDialog hint", "确定", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }).show();
+                break;
+            case R.id.show_image_header_1btn:
+
+                DialogCreator.showImageHeader1BtnDialog(this, url, "showImageHeader1BtnDialog", "showEditTitle1BtnDialog msg", "确定", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }).show();
+                break;
+            case R.id.show_logo_header_1btn:
+
+                DialogCreator.showLogoHeader1BtnDialog(this, url, "利宝特权说明", "showLogoHeader1BtnDialog ", "确定", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }).show();
+                break;
+            case R.id.show_card_2btn:
+
+                DialogCreator.showCard2BtnDialog(this, "showCard2BtnDialog msg", url, "title暗示法大是大非未完全范德萨发的份额范德萨", "70.7包邮(100.7-30)", "去看看", new OnLeftListener() {
+                    @Override
+                    public void onLeftClick(String str) {
+
+                    }
+                }, "换一个", new OnRightListener() {
+                    @Override
+                    public void onRightClick(String str) {
+
+                    }
+                }).show();
+
+                break;
+
+
+            default:
+                break;
+        }
+
+
+    }
+}
---
+++ dialoglib/src/main/java/com/ccj/dialoglib/dialog/CommonImgHeaderDialog.java
@@ -0,0 +1,71 @@
+package com.ccj.dialoglib.dialog;
+
+import android.content.Context;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.ccj.dialoglib.DialogConfig;
+import com.ccj.dialoglib.R;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+/**
+ * 通用 带头部背景 样式
+ * Created by chenchangjun on 17/10/11.
+ */
+
+public class CommonImgHeaderDialog extends CommonNormalDialog implements View.OnClickListener {
+
+    private String url_header;
+    private ImageView iv_header_cancel;
+    private SimpleDraweeView iv_header;
+
+
+    @Override
+    protected void loadHeader() {
+
+
+        if (type== DialogConfig.TYPE_HAS_HEADER){
+            iv_header.setVisibility(View.VISIBLE);
+            iv_header_cancel.setVisibility(View.VISIBLE);
+
+
+            if (TextUtils.isEmpty(url_header)) {
+                iv_header.setVisibility(View.GONE);
+            } else {
+                //iv_header.setImageURI(url_header);
+                iv_header.setBackgroundResource(R.drawable.common_dialog_header_bg);
+                Uri uri = Uri.parse(url_header);
+                iv_header.setImageURI(uri);
+            }
+
+
+
+        }
+    }
+
+    public CommonImgHeaderDialog setHeaderUrl(String str) {
+        url_header = str;
+        return this;
+    }
+
+    public CommonImgHeaderDialog(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected View initHeader() {
+        View header=null;
+        if (type==DialogConfig.TYPE_HAS_NO_HEADER){
+            return null;
+        }
+        header = View.inflate(context, R.layout.common_dialog_pic_header, null);
+        iv_header_cancel = (ImageView) header.findViewById(R.id.iv_header_cancel);
+        iv_header = (SimpleDraweeView) header.findViewById(R.id.iv_header);
+        iv_header_cancel.setOnClickListener(this);
+
+        return header;
+    }
+
+}
